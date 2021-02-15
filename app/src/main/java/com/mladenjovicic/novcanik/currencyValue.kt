4e989d79561359567a24cfb18d525f39cb71cbb1
mladenjovicic <mladen.jovicic@outlook.com>
2021-02-15 11:01:27 +0100

    valute

---
+++ app/src/main/java/com/mladenjovicic/novcanik/currencyValue.kt
@@ -0,0 +1,6 @@
+package com.mladenjovicic.novcanik
+
+class currencyValue(var dateCurrency:String ="", var BAM:Double=0.0, var RSD:Double=0.0,var HRK:Double=0.0, var USD:Double=0.0, var EUR:Double=0.0, var GBP:Double=0.0,
+                    var CHF:Double=0.0, var JPY:Double=0.0, var AUD:Double=0.0, var CAD:Double=0.0, var RUB:Double=0.0, var CNY:Double=0.0) {
+    var id:Int = 0
+}
